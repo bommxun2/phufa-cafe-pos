612e02fb9559883158586e54facb15ef60a8c1bf
krittamark <33484828+krittamark@users.noreply.github.com>
2025-04-20 02:59:05 +0700

    fix: rename file order, report to conventional format

--- backend/src/routes/order.route.js
+++ backend/src/routes/order.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getEmployeeOrders } = require('../controllers/order.controller');
+const getEmployeeOrders = require('../controllers/order/getEmployeeOrders.controller');
 
 // Get all orders for a specific employee
 router.get('/employees/:empId/orders', getEmployeeOrders);
