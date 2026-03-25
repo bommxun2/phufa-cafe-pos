b1ec3b4bfa0c2a8fa83660f41fa767dc48ef8aaa
Bommxun2 <wachirakorn.cn@gmail.com>
2026-03-25 22:35:21 +0700

    edit how to install opentofu in .sh

--- terraform/tools-install.sh
+++ terraform/tools-install.sh
@@ -3,7 +3,7 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 # Update OS packages
 dnf update -y
-dnf install -y wget curl unzip
+dnf install -y git wget curl unzip
 
 # Install java 21
 dnf install -y java-21-amazon-corretto-devel
@@ -58,7 +58,30 @@ EOF
 dnf install -y trivy
 
 # Download and Run OpenTofu install script
-curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o /tmp/install-opentofu.sh
-chmod +x /tmp/install-opentofu.sh
-/tmp/install-opentofu.sh --install-method standalone
-rm -f /tmp/install-opentofu.sh
+sudo tee /etc/yum.repos.d/opentofu.repo > /dev/null <<'EOF'
+[opentofu]
+name=opentofu
+baseurl=https://packages.opentofu.org/opentofu/tofu/rpm_any/rpm_any/\$basearch
+repo_gpgcheck=0
+gpgcheck=1
+enabled=1
+gpgkey=https://get.opentofu.org/opentofu.gpg
+       https://packages.opentofu.org/opentofu/tofu/gpgkey
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+metadata_expire=300
+
+[opentofu-source]
+name=opentofu-source
+baseurl=https://packages.opentofu.org/opentofu/tofu/rpm_any/rpm_any/SRPMS
+repo_gpgcheck=0
+gpgcheck=1
+enabled=1
+gpgkey=https://get.opentofu.org/opentofu.gpg
+       https://packages.opentofu.org/opentofu/tofu/gpgkey
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+metadata_expire=300
+EOF
+sudo yum update
+sudo yum install -y tofu
